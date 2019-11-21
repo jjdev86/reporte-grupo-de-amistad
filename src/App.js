50f2720622b6bc2a3d79d2560549dfd862d7d025
Jose Valadez <vinmajj@gmail.com>
2019-11-20 20:21:38 -0800

    Update App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 // import General from './components/general';
-import General1 from './components/gernal1';
-import Members from './components/members';
-import 'bootstrap/dist/css/bootstrap.css';
-import Axios from 'axios';
+import General1 from "./components/gernal1";
+import Members from "./components/members";
+import "bootstrap/dist/css/bootstrap.css";
+import Axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      userID: ''
-    }
+      userID: ""
+    };
   }
 
   Save(data) {
     // make request to server to save form
-
   }
   // test
   render() {
@@ -28,25 +27,21 @@ class App extends Component {
             <h1> Groupos de Amistad - Iglesia Fuego Pentecostes</h1>
           </div>
         </div>
-        <div style={{ clear: 'left', backgroundColor: '#F1F4F7', overflow: 'hidden' }}>
+        <div
+          style={{
+            clear: "left",
+            backgroundColor: "#F1F4F7",
+            overflow: "hidden"
+          }}
+        >
           <div className="" style={{ color: "#676570" }}>
-            {/* <header className="App-header"> */}
-            <form name="event_report" encType="multipart/form-data" method="post" >
-              <ul className="col-lg-12 col-md-12">
-                <fieldset>
-                  <li style={{ listStyle: 'none' }}>
-                    <General1 Save={this.Save.bind(this)} />
-                  </li>
-                  <li style={{ listStyle: 'none' }}>
-                    <Members Save={this.Save.bind(this)} />
-                  </li>
-                </fieldset>
-              </ul>
-            </form>
-            {/* </header> */}
+            <General1 Save={this.Save.bind(this)} />
+            <Members Save={this.Save.bind(this)} />
           </div>
           <div className="col-lg-12 col-md-12 col-xs-12 btnsection">
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </div>
         </div>
       </div>
